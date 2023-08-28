334b3d250365fc0d6e4ef9fea0642c398bef05c4
pmscode96 <mscode@gmail.com>
2023-08-28 15:51:30 +0900

    쿠폰함

---
+++ pms/couponlist.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('.parkingmate-downloadItem-more');
+// let hidden = document.querySelector('.hidden');
+let target = document.querySelector('parkingmate-couponDownloadItem-expand');
+let opne = document.querySelector('oepn');
+let close = document.querySelector('close');
+
+
+button.addEventListener('click',() =>{
+  console.log("ㅁㄴㅇㄹ");
+  target.classList.toggle('hidden');
+
+})
+
+.toggleClass();
