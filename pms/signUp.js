d3343c6f6993629697316eabc698c8b95a7568d9
pmscode96 <mscode96@gmail.com>
2023-09-03 11:45:36 +0900

    쿠폰함js

---
+++ pms/signUp.js
@@ -0,0 +1,8 @@
+let $button = $(".hide-button");
+let $modal = $(".modal-container");
+$button.on("click", (e) =>{
+  console.log("asdf")
+  $modal.css("display","block");
+  // $modal.slideToggle(1000);
+
+})
