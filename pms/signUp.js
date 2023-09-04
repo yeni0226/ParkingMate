2eed08b8f164fc0fc289ed6b71d218abb82dc00b
pmscode96 <mscode@gmail.com>
2023-09-04 12:52:16 +0900

    최종

--- pms/signUp.js
+++ pms/signUp.js
@@ -1,8 +1,16 @@
-let $button = $(".hide-button");
+let $button = $(".button-text");
 let $modal = $(".modal-container");
-$button.on("click", (e) =>{
-  console.log("asdf")
-  $modal.css("display","block");
-  // $modal.slideToggle(1000);
+let $body = $("body");
+let $close = $(".modal-close")
 
+// 이용약관 표시
+$button.on("click", (e) =>{
+  $modal.css("display","block");
+  $body.css("overflow", "hidden");
+});
+
+// 이용약관 끄기
+$close.on("click", (e) =>{
+  $modal.css("display", "none");
+  $body.css("overflow","auto");
 })
