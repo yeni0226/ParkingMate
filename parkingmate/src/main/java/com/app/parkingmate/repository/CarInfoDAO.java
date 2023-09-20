c98c622bc29190f4266765f8eca97d6f982f0d95
projectJun <projectJun6460@gmail.com>
2023-09-20 17:30:21 +0900

    프로젝트 패키지 경로, 패키지 세부내역 excel 파일, MyBatis 설정 완료

---
+++ parkingmate/src/main/java/com/app/parkingmate/repository/CarInfoDAO.java
@@ -0,0 +1,9 @@
+package com.app.parkingmate.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CarInfoDAO {
+}
---
+++ parkingmate/src/main/java/com/app/parkingmate/repository/CardInfoDAO.java
@@ -0,0 +1,10 @@
+package com.app.parkingmate.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CardInfoDAO {
+
+}
